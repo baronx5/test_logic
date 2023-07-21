4ac120e83fd16fa31ea2e867f471c404abf050c3
baronx5 <baron1574@gmail.com>
2023-07-21 15:18:41 +0300

    Navigationhomebutton

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,87 @@
 import 'package:flutter/material.dart';
-import 'package:test_logic/screen/list_of_best_sales.dart';
-import 'package:test_logic/screen/welcome_msg.dart';
 import 'Theme/app_theme.dart';
-import 'menu.dart';
+import 'main_home_bage.dart';
 
 void main() {
-  runApp(const MyHomePage());
+  runApp(const MainAppNavigationBage());
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+class MainAppNavigationBage extends StatelessWidget {
+  const MainAppNavigationBage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.lightTheme(),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'السوق العربي',
+    return const MaterialApp(
+      home: MainNavigation(),
+    );
+  }
+}
+
+class MainNavigation extends StatefulWidget {
+  const MainNavigation({super.key});
+
+  @override
+  State<MainNavigation> createState() => _MainNavigationState();
+}
+
+class _MainNavigationState extends State<MainNavigation> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    MyHomePage(),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: Settings',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.home),
+            label: 'Home',
+            backgroundColor: AppTheme.colors.color4,
           ),
-          leading: const Icon(Icons.shopping_bag_outlined),
-        ),
-        body: ListView(
-          padding: const EdgeInsets.all(10.0),
-          scrollDirection: Axis.vertical,
-          children: const [
-            WelcomeMsg(),
-            HomeMenu(),
-            ListBestSales(),
-          ],
-        ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+            backgroundColor: Colors.green,
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+            backgroundColor: Colors.purple,
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+            backgroundColor: Colors.pink,
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: AppTheme.colors.color0,
+        onTap: _onItemTapped,
       ),
     );
   }

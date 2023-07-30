8e7433d7062fe58703011fb3804c1127b52cb73f
baronx5 <baron1574@gmail.com>
2023-07-30 15:53:01 +0300

    fixing layout.

--- lib/screen/profile_page.dart
+++ lib/screen/profile_page.dart
@@ -6,7 +6,7 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('السوق العربي')),
+      appBar: AppBar(title: const Text('حسابي ')),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -23,7 +23,32 @@ class ProfilePage extends StatelessWidget {
           ),
         ),
 
-        const Text('Welcome Name', textAlign: TextAlign.center,)
+        const Text('مرحبا بك .. حسين', textAlign: TextAlign.center,),
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: 10.0),
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            border: Border(bottom: BorderSide(color: Colors.black12)),
+          ),
+          child:  ListTile(
+            title: Text('adress comes here for example', style: Theme.of(context).textTheme.bodyMedium,),
+            leading: const Icon(Icons.card_giftcard),
+            trailing: const Icon(Icons.menu),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: 10.0),
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            border: Border(bottom: BorderSide(color: Colors.black12)),
+          ),
+          child:  ListTile(
+            title: Text('Hello', style: Theme.of(context).textTheme.bodyMedium,),
+            leading: const Icon(Icons.card_giftcard),
+            trailing: const Icon(Icons.menu),
+          ),
+        ),
+
       ]),
     );
   }
